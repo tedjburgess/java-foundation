8728ee77f4abfe996be270a9ee8ee20e25b64456
Ted J Burgess <ted.j.burgess@gmail.com>
2025-12-28 17:09:40 +0100

    Added status to ToDo object Added the following -status to ToDo constructor -updated getToDo to show status

--- src/command_line_todo_list/ToDo.java
+++ src/command_line_todo_list/ToDo.java
@@ -3,15 +3,24 @@ package command_line_todo_list;
 public class ToDo {
     String name;
     String description;
+    boolean status;
     
-    public ToDo(String name, String description) {
+    public ToDo(String name, String description, boolean status) {
         this.name = name;
         this.description = description;
+        this.status = status;
     }
     public String getToDo() {
-        return "Name: " + name + "\n"
+        String statusAsii = "❌";
+        if (status == true) {
+            statusAsii = "✅";
+        }
+        return "Name: " + name + statusAsii + "\n"
         + "Description: " + description + "\n";
     }
+    // public boolean finishTodo(boolean finished) {
+    //     return(finished);
+    // }
     // public String getName() {
     //     return name;
     // }
